4f5e015299cb3694ef1f8000ddbf509c6f567ff7
wisadev <supachai.wi@gmail.com>
2018-02-10 17:53:01 +0700

    new

---
+++ application/views/contact.php
@@ -0,0 +1,53 @@
+
+<section class="contuct-us-form-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12">
+                    <div class="area-title">
+                        <h2>ติดต่อเรา</h2>
+                    </div>
+                </div>
+            </div>
+            <div style="padding-top: 20px;"></div>
+            <div class="row">
+
+            <div class="col-sm-6 col-lg-6 col-md-6">
+                <div class="our-team">
+                    <h2 class="sub-section-title">
+                        xcitehitec (เอ็กไซ ไฮเทค)
+                    </h2>
+                    <address>
+                        <p><i class="fa fa-map-marker"></i>  บริษัท เอ็กไซ ไฮเทค จำกัด <br>ห้อง 3R036-037 ชั้น 3 ตึกคอมโฆษะขอนแก่น ถนนศรีจันทร์  ตำบลในเมือง อำเภอเมือง จังหวัดขอนแก่น 40000</p>
+                        <p><i class="fa fa-phone"></i> Mobile: 087-8809644 , TEL. 043-389438</p>
+                        <p><i class="fa fa-envelope-o"></i> Email: <?php echo $this->config->item('email_owner') ?></p>
+                        <p><i class="fa fa-comment"></i> Line Id: <a href="http://line.me/ti/p/%40xcitehitec" target="_blank">@xcitehitec</a></p>
+                    </address>
+                </div>
+                  <div id="fb-root"></div>
+                    <script>(function(d, s, id) {
+                      var js, fjs = d.getElementsByTagName(s)[0];
+                      if (d.getElementById(id)) return;
+                      js = d.createElement(s); js.id = id;
+                      js.src = "//connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v2.6&appId=615663091936505";
+                      fjs.parentNode.insertBefore(js, fjs);
+                    }(document, 'script', 'facebook-jssdk'));</script>
+                        <div class="add-banner-carsuol">
+                            <div class="fb-page" data-href="https://www.facebook.com/xcitehiteccom-259231204578178/" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+                                <blockquote cite="https://www.facebook.com/xcitehiteccom-259231204578178/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/xcitehiteccom-259231204578178/">xcitehitec.com</a></blockquote>
+                            </div>
+                        </div>
+            </div>
+            <div class="col-sm-6 col-lg-6 col-md-6">
+                <div class="our-team">
+                    <h2 class="sub-section-title">
+                        xcitehitec (เอ็กไซ ไฮเทค)
+                    </h2>
+                </div>
+                <img src="<?php echo base_url('theme');?>/img/xc_ct1.jpg"  class="img-responsive" alt="">
+            </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    </section>
+    <div style="padding-top: 50px;"></div>
---
+++ application/views/payment.php
@@ -0,0 +1,111 @@
+<section class="contuct-us-form-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12">
+                <!-- breadcrumbs start-->
+                <div class="breadcrumb">
+                    <ul>
+                        <li>
+                            <a href="<?php echo base_url(); ?>">Home</a>
+                            <i class="fa fa-angle-right"></i>
+                        </li>
+                        <li>แจ้งชำระเงิน</li>
+                    </ul>
+                </div>
+                <!-- breadcrumbs end-->
+            </div>
+        </div>
+        <div class="" style="padding-top:30px;">
+            <div class="row">
+                <div class="col-sm-6">
+                <?php if (isset($s_error)): ?>
+                     <?php if ($is_error): ?>
+                        <p class="texe-danger">
+                             <?php echo $txt_res;?>
+                        </p>
+
+                    <?php else: ?>
+                        <p class="text-success">
+                            <?php echo $txt_res;?>
+                        </p>
+
+                    <?php endif ?>
+                <?php endif ?>
+                <?php echo form_open_multipart('payment/save');?>
+                <fieldset>
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label for="textinput">ชื่อ</label>
+                            <input id="textinput" name="txtName" type="text" placeholder="ชื่อ" class="form-control input-md" required="required">
+                        </div>
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label for="textinput">เบอร์ติดต่อ</label>
+                            <input id="textinput" name="txtTel" type="text" placeholder="เบอร์ติดต่อ" class="form-control input-md" required="required">
+                        </div>
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <label for="textinput">เลขที่ใบสั่งซื้อ</label>
+                            <input id="textinput"  name="txtOrder" type="text" placeholder="เลขที่ใบสั่งซื้อ" class="form-control input-md" required="required">
+                        </div>
+                        <div class="form-group">
+                            <label for="textinput">เลือกธนาคาร</label>
+                            <select  name="txtBank" id="inputBank" class="form-control" required="required">
+                                <option value="ธนาคารไทยพาณิชย์">ธนาคารไทยพาณิชย์</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="textinput">จำนวนเงิน</label>
+                            <input id="textinput"  name="txtAmount" type="text" placeholder="จำนวนเงิน" class="form-control input-md" required="required">
+                        </div>
+                        <div class="form-group">
+                            <label for="textinput">วันที่โอน ตัวอย่าง 01/04/2016</label>
+                            <input id="textinput"  name="txtDate" type="text" placeholder="วันที่โอน" class="form-control input-md" required="required">
+                        </div>
+                        <div class="form-group">
+                            <label for="textinput">เวลาโอน ตัวอย่าง 12:00</label>
+                            <input id="textinput" name="txtTime" type="text" placeholder="เวลาโอน" class="form-control input-md" required="required">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="textinput">แนบไฟล์</label>
+                            <input id="userfile" name="userfile" type="file" class="form-control input-md" required="required">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="filebutton"></label>
+                            <button type="submit" value="upload"  class="btn btn-success ">แจ้งชำระ</button>
+                        </div>
+
+
+                </fieldset>
+
+                </form>
+
+                </div>
+                <div class="col-sm-6">
+                    <div class="row bank-payment">
+                        <div class="col-sm-3" style=" margin:0 auto;">
+                            <img src="<?php echo base_url('theme'); ?>/img/thaipanit.jpg" class="img-responsive img-rounded" alt="Image">
+                        </div>
+                        <div class="col-sm-9">
+                            <h4>ธนาคารไทยพาณิชย์</h4>
+                            <p>เลขที่บัญชี : 935-229047-8
+                                <br> ชื่อบัญชี : บริษัท เอ็กไซ ไฮเทค จำกัด
+                                <br>
+                        </div>
+                    </div>
+                    <div class="" style="padding-top:30px;">
+                        <p><strong>แนบสลิปผ่านทางเว็บไซต์</strong></p>
+                    </div>
+                    <div class="">
+                        <p><strong>แจ้งชำระเงินผ่านทาง line : </strong>
+                            <span class="fa fa-comment"></span> LINE ID : <?php echo $this->config->item('line_id') ?>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<div style="padding-top: 50px;"></div>
